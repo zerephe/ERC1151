5f6df07da0da66251e6e622ee9fbd013e908fc88
zerephe <zerephe97@gmail.com>
2022-05-05 14:23:06 +0600

    access controle

--- contracts/My1155.sol
+++ contracts/My1155.sol
@@ -2,11 +2,14 @@
 pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Supply.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 
-contract NumberCollectible2 is ERC1155, Ownable, ERC1155Supply {
+contract NumberCollectible2 is ERC1155, ERC1155Supply, AccessControl {
+    bytes32 public constant URI_SETTER_ROLE = keccak256("URI_SETTER_ROLE");
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    
     uint256 public constant ONE = 0;
     uint256 public constant TWO = 1;
     string private _baseUri = "https://ipfs.io/ipfs/QmV4KdKfFTV8MPkj9ntW41RMY22MgUbrGyKsegi1Ax5P9V/{id}.json";
@@ -16,23 +19,26 @@ contract NumberCollectible2 is ERC1155, Ownable, ERC1155Supply {
     constructor() ERC1155(_baseUri) {
         _mint(msg.sender, ONE, 1, "");
         _mint(msg.sender, TWO, 1, "");
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(URI_SETTER_ROLE, msg.sender);
+        _grantRole(MINTER_ROLE, msg.sender);
 
         supportedInterfaces[type(IERC1155).interfaceId] = true;
     }
 
-    function setURI(string memory newuri) public onlyOwner {
+    function setURI(string memory newuri) public onlyRole(URI_SETTER_ROLE) {
         _setURI(newuri);
     }
 
-    function supportsInterface(bytes4 interfaceId) public view override returns (bool) {
+    function supportsInterface(bytes4 interfaceId) public view override(ERC1155, AccessControl) returns (bool) {
         return supportedInterfaces[interfaceId];
     }
 
-    function mint(address account, uint256 id, uint256 amount, bytes memory data) external onlyOwner {
+    function mint(address account, uint256 id, uint256 amount, bytes memory data) external onlyRole(MINTER_ROLE){
         _mint(account, id, amount, data);
     }
 
-    function mintBatch(address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data) external onlyOwner {
+    function mintBatch(address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data) external onlyRole(MINTER_ROLE){
         _mintBatch(to, ids, amounts, data);
     }
 
