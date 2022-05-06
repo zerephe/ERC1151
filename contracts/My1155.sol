53acca55cf73e7ddd0e98afa70e4b248e0db088b
zerephe <zerephe97@gmail.com>
2022-05-06 14:38:33 +0600

    changes: mint with uri

--- contracts/My1155.sol
+++ contracts/My1155.sol
@@ -1,24 +1,19 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155URIStorage.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
-import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Supply.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 
-contract NumberCollectible2 is ERC1155, ERC1155Supply, AccessControl {
+contract NumberCollectible2 is ERC1155URIStorage, AccessControl {
     bytes32 public constant URI_SETTER_ROLE = keccak256("URI_SETTER_ROLE");
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
     
-    uint256 public constant ONE = 0;
-    uint256 public constant TWO = 1;
     string private _baseUri = "https://ipfs.io/ipfs/QmV4KdKfFTV8MPkj9ntW41RMY22MgUbrGyKsegi1Ax5P9V/{id}.json";
-
+    
     mapping(bytes4 => bool) internal supportedInterfaces;
 
     constructor() ERC1155(_baseUri) {
-        _mint(msg.sender, ONE, 1, "");
-        _mint(msg.sender, TWO, 1, "");
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
         _grantRole(URI_SETTER_ROLE, msg.sender);
         _grantRole(MINTER_ROLE, msg.sender);
@@ -26,26 +21,32 @@ contract NumberCollectible2 is ERC1155, ERC1155Supply, AccessControl {
         supportedInterfaces[type(IERC1155).interfaceId] = true;
     }
 
-    function setURI(string memory newuri) public onlyRole(URI_SETTER_ROLE) {
-        _setURI(newuri);
+    function setURI(string memory newuri) internal pure returns(string memory) {
+        return string(abi.encodePacked(newuri, "/{id}.json"));
     }
 
     function supportsInterface(bytes4 interfaceId) public view override(ERC1155, AccessControl) returns (bool) {
         return supportedInterfaces[interfaceId];
     }
 
-    function mint(address account, uint256 id, uint256 amount, bytes memory data) external onlyRole(MINTER_ROLE){
-        _mint(account, id, amount, data);
+    function mint(address to, uint256 id, uint256 amount, bytes memory data) external onlyRole(MINTER_ROLE){
+        _mint(to, id, amount, data);
+    }
+
+    function mintWithURI(string memory _uri, address to, uint256 id, uint256 amount, bytes memory data) external onlyRole(MINTER_ROLE){
+        _mint(to, id, amount, data);
+        _setURI(id, _uri);
     }
 
     function mintBatch(address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data) external onlyRole(MINTER_ROLE){
         _mintBatch(to, ids, amounts, data);
     }
 
-    function _beforeTokenTransfer(address operator, address from, address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data)
-        internal
-        override(ERC1155, ERC1155Supply)
-    {
-        super._beforeTokenTransfer(operator, from, to, ids, amounts, data);
+    function mintBatchWithURI(string[] memory _uri, address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data) external onlyRole(MINTER_ROLE){
+        _mintBatch(to, ids, amounts, data);
+
+        for(uint256 i = 0; i < ids.length; i++){
+            _setURI(ids[i], setURI(_uri[i]));
+        }
     }
 }
--- test/unit/test1155.ts
+++ test/unit/test1155.ts
@@ -22,35 +22,39 @@ describe("NumberCollectible NFTs", function () {
       expect(nftInstance.address).to.be.properAddress;
     });
 
-    it("Should have proper initial values", async function() {
-      expect(await nftInstance.ONE()).to.eq(0);
-      expect(await nftInstance.TWO()).to.eq(1);
-      expect(await nftInstance.totalSupply(0)).to.eq(1);
-      expect(await nftInstance.totalSupply(1)).to.eq(1);
+    it("Should should support 1155 interface", async function() {
+      expect(await nftInstance.supportsInterface(0xd9b67a26)).to.eq(true);
     });
   });
 
   describe("Txs", function() {
     it("Should have some nft after minting and total supply change", async function() {
       await nftInstance.mint(owner.address, 0, 1, "0x00");
-      expect(await nftInstance.balanceOf(owner.address, 0)).to.eq(2);
-      expect(await nftInstance.totalSupply(0)).to.eq(2);
+      expect(await nftInstance.balanceOf(owner.address, 0)).to.eq(1);
     });
 
     it("Should have some nft after batch minting and total supply change", async function() {
       const ids = [0, 1];
       const amounts = [1, 1];
       await nftInstance.mintBatch(owner.address, ids, amounts, "0x00");
-      expect(await nftInstance.balanceOf(owner.address, 0)).to.eq(2);
-      expect(await nftInstance.balanceOf(owner.address, 1)).to.eq(2);
-      expect(await nftInstance.totalSupply(0)).to.eq(2);
-      expect(await nftInstance.totalSupply(1)).to.eq(2);
+      expect(await nftInstance.balanceOf(owner.address, 0)).to.eq(1);
+      expect(await nftInstance.balanceOf(owner.address, 1)).to.eq(1);
     });
 
-    it("Should should be possible to change URI", async function() {
-      await nftInstance.setURI("uri");
-      expect(await nftInstance.uri(0)).to.eq("uri0.json")
-      expect(await nftInstance.uri(1)).to.eq("uri1.json")
+    it("Should have some nft after minting with uri and total supply, uri change", async function() {
+      await nftInstance.mintWithURI("new uri",owner.address, 0, 1, "0x00");
+      expect(await nftInstance.balanceOf(owner.address, 0)).to.eq(1);
+      expect(await nftInstance.uri(0)).to.eq("new uri");
+    });
+
+    it("Should have some nft after batch minting and total supply change", async function() {
+      const ids = [0, 1];
+      const amounts = [1, 1];
+      await nftInstance.mintBatchWithURI(["new uri", "two uri"], owner.address, ids, amounts, "0x00");
+      expect(await nftInstance.balanceOf(owner.address, 0)).to.eq(1);
+      expect(await nftInstance.balanceOf(owner.address, 1)).to.eq(1);
+      expect(await nftInstance.uri(0)).to.eq("new uri/{id}.json");
+      expect(await nftInstance.uri(1)).to.eq("two uri/{id}.json");
     });
   });
 });
